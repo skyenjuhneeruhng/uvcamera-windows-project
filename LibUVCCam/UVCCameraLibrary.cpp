27ef3f3f2eb8ef6963c1ec1c055092f020344e1d
sky19938470 <46250385+sky19938470@users.noreply.github.com>
2020-06-04 09:24:54 -0400

    osd menu tool added

--- LibUVCCam/UVCCameraLibrary.cpp
+++ LibUVCCam/UVCCameraLibrary.cpp
@@ -1,6 +1,308 @@
 #include "stdafx.h"
 #include "UVCCameraLibrary.h"
 
+/**********************************************Implementation of UVCXU class*********************************************************/
+UVCXU::UVCXU()
+{
+	SAFE_RELEASE(m_pKsControl);
+	m_pKsControl = NULL;
+	m_dwNodeId = 0;
+	UvcXuGudi = { 0 };
+}
+
+UVCXU::~UVCXU()
+{
+	SAFE_RELEASE(m_pKsControl);
+}
+
+
+
+HRESULT
+UVCXU::QueryUvcXuInterface(
+	IBaseFilter* pDeviceFilter,
+	GUID extensionGuid,
+	DWORD FirstNodeId)
+{
+	HRESULT hr = S_OK;
+	IKsTopologyInfo* pIksTopologyInfo = NULL;
+	DWORD numberOfNodes;
+	KSP_NODE ExtensionProp;
+
+	if (pDeviceFilter == NULL)
+		return E_POINTER;
+
+
+	hr = pDeviceFilter->QueryInterface(__uuidof(IKsTopologyInfo), (void**)&pIksTopologyInfo);
+	if (FAILED(hr))
+		return hr;
+
+
+	hr = pIksTopologyInfo->get_NumNodes(&numberOfNodes);
+	if (FAILED(hr) || numberOfNodes == 0)
+	{
+		SAFE_RELEASE(pIksTopologyInfo);
+		return E_FAIL;
+	}
+
+	DWORD i = 0, j = 0;
+	GUID nodeGuid;
+	if (FirstNodeId < numberOfNodes)
+		i = FirstNodeId;
+
+	for (; i < numberOfNodes; i++)
+	{
+		if (SUCCEEDED(pIksTopologyInfo->get_NodeType(i, &nodeGuid)))
+		{
+			if (IsEqualGUID(KSNODETYPE_DEV_SPECIFIC, nodeGuid))
+			{
+#if _DEBUG
+				printf("found one xu node, NodeId = %d\n", i);
+#endif
+				SAFE_RELEASE(m_pKsControl);
+				m_pKsControl = NULL;
+				hr = pIksTopologyInfo->CreateNodeInstance(i, IID_IKsControl, (void**)&m_pKsControl);
+				if (SUCCEEDED(hr))
+				{
+					ULONG ulBytesReturned;
+					ExtensionProp.Property.Set = extensionGuid;
+					ExtensionProp.Property.Id = KSPROPERTY_EXTENSION_UNIT_INFO;
+					ExtensionProp.Property.Flags = KSPROPERTY_TYPE_SETSUPPORT | KSPROPERTY_TYPE_TOPOLOGY;
+					ExtensionProp.NodeId = i;
+					ExtensionProp.Reserved = 0;
+
+					hr = m_pKsControl->KsProperty(
+						(PKSPROPERTY)&ExtensionProp,
+						sizeof(ExtensionProp),
+						NULL,
+						0,
+						&ulBytesReturned);
+
+					if (hr == S_OK)
+					{
+#if _DEBUG						
+						printf("CreateNodeInstance NodeId = %d\n", i);
+#endif
+						m_dwNodeId = i;
+						UvcXuGudi = extensionGuid;
+						return hr;
+					}
+				}
+				else
+				{
+#if _DEBUG
+					printf("CreateNodeInstance failed - 0x%x, NodeId = %d\n", hr, i);
+#endif
+				}
+			}
+		}
+
+	}
+
+	if (i == numberOfNodes)
+	{	// Did not find the node
+		SAFE_RELEASE(m_pKsControl);
+		m_pKsControl = NULL;
+		hr = E_FAIL;
+	}
+
+	SAFE_RELEASE(pIksTopologyInfo);
+	return hr;
+}
+
+HRESULT
+UVCXU::FinalConstruct()
+{
+	if (m_pKsControl == NULL) return E_FAIL;
+	return S_OK;
+}
+
+HRESULT
+UVCXU::get_InfoSize(
+	ULONG* pulSize)
+{
+	HRESULT hr = S_OK;
+	ULONG ulBytesReturned;
+	KSP_NODE ExtensionProp;
+
+	if (!pulSize || m_pKsControl == NULL) return E_POINTER;
+
+	ExtensionProp.Property.Set = UvcXuGudi;
+	ExtensionProp.Property.Id = KSPROPERTY_EXTENSION_UNIT_INFO;
+	ExtensionProp.Property.Flags = KSPROPERTY_TYPE_GET |
+		KSPROPERTY_TYPE_TOPOLOGY;
+	ExtensionProp.NodeId = m_dwNodeId;
+
+	hr = m_pKsControl->KsProperty(
+		(PKSPROPERTY)&ExtensionProp,
+		sizeof(ExtensionProp),
+		NULL,
+		0,
+		&ulBytesReturned);
+
+	if (hr == HRESULT_FROM_WIN32(ERROR_MORE_DATA))
+	{
+		*pulSize = ulBytesReturned;
+		hr = S_OK;
+	}
+
+	return hr;
+}
+
+
+HRESULT
+UVCXU::get_Info(
+	ULONG ulSize,
+	BYTE pInfo[])
+{
+	HRESULT hr = S_OK;
+	KSP_NODE ExtensionProp;
+	ULONG ulBytesReturned;
+
+	if (m_pKsControl == NULL) return E_POINTER;
+
+	ExtensionProp.Property.Set = UvcXuGudi;
+	ExtensionProp.Property.Id = KSPROPERTY_EXTENSION_UNIT_INFO;
+	ExtensionProp.Property.Flags = KSPROPERTY_TYPE_GET |
+		KSPROPERTY_TYPE_TOPOLOGY;
+	ExtensionProp.NodeId = m_dwNodeId;
+
+	hr = m_pKsControl->KsProperty(
+		(PKSPROPERTY)&ExtensionProp,
+		sizeof(ExtensionProp),
+		(PVOID)pInfo,
+		ulSize,
+		&ulBytesReturned);
+
+	return hr;
+}
+
+
+HRESULT
+UVCXU::get_PropertySize(
+	ULONG PropertyId,
+	ULONG* pulSize)
+{
+	HRESULT hr = S_OK;
+	ULONG ulBytesReturned;
+	KSP_NODE ExtensionProp;
+
+	if (!pulSize || m_pKsControl == NULL) return E_POINTER;
+
+	ExtensionProp.Property.Set = UvcXuGudi;
+	ExtensionProp.Property.Id = PropertyId;
+	ExtensionProp.Property.Flags = KSPROPERTY_TYPE_GET |
+		KSPROPERTY_TYPE_TOPOLOGY;
+	ExtensionProp.NodeId = m_dwNodeId;
+
+	hr = m_pKsControl->KsProperty(
+		(PKSPROPERTY)&ExtensionProp,
+		sizeof(ExtensionProp),
+		NULL,
+		0,
+		&ulBytesReturned);
+
+	if (hr == HRESULT_FROM_WIN32(ERROR_MORE_DATA))
+	{
+		*pulSize = ulBytesReturned;
+		hr = S_OK;
+	}
+
+	return hr;
+}
+
+HRESULT
+UVCXU::get_Property(
+	ULONG PropertyId,
+	ULONG ulSize,
+	BYTE pValue[])
+{
+	HRESULT hr = S_OK;
+	KSP_NODE ExtensionProp;
+	ULONG ulBytesReturned;
+
+	if (m_pKsControl == NULL) return E_POINTER;
+
+	ExtensionProp.Property.Set = UvcXuGudi;
+	ExtensionProp.Property.Id = PropertyId;
+	ExtensionProp.Property.Flags = KSPROPERTY_TYPE_GET |
+		KSPROPERTY_TYPE_TOPOLOGY;
+	ExtensionProp.NodeId = m_dwNodeId;
+	ExtensionProp.Reserved = 0;
+
+	hr = m_pKsControl->KsProperty(
+		(PKSPROPERTY)&ExtensionProp,
+		sizeof(ExtensionProp),
+		(PVOID)pValue,
+		ulSize,
+		&ulBytesReturned);
+
+	if (ulSize != ulBytesReturned)
+		return E_FAIL;
+
+	return hr;
+}
+
+HRESULT
+UVCXU::put_Property(
+	ULONG PropertyId,
+	ULONG ulSize,
+	BYTE pValue[])
+{
+	HRESULT hr = S_OK;
+	KSP_NODE ExtensionProp;
+	ULONG ulBytesReturned;
+
+	if (m_pKsControl == NULL) return E_POINTER;
+
+	ExtensionProp.Property.Set = UvcXuGudi;
+	ExtensionProp.Property.Id = PropertyId;
+	ExtensionProp.Property.Flags = KSPROPERTY_TYPE_SET |
+		KSPROPERTY_TYPE_TOPOLOGY;
+	ExtensionProp.NodeId = m_dwNodeId;
+	ExtensionProp.Reserved = 0;
+
+	hr = m_pKsControl->KsProperty(
+		(PKSPROPERTY)&ExtensionProp,
+		sizeof(ExtensionProp),
+		(PVOID)pValue,
+		ulSize,
+		&ulBytesReturned);
+
+	return hr;
+}
+
+typedef struct {
+	KSPROPERTY_MEMBERSHEADER    MembersHeader;
+	const VOID* Members;
+} KSPROPERTY_MEMBERSLIST, * PKSPROPERTY_MEMBERSLIST;
+
+typedef struct {
+	KSIDENTIFIER                    PropTypeSet;
+	ULONG                           MembersListCount;
+	_Field_size_(MembersListCount)
+		const KSPROPERTY_MEMBERSLIST* MembersList;
+} KSPROPERTY_VALUES, * PKSPROPERTY_VALUES;
+
+HRESULT
+UVCXU::get_PropertyRange(
+	ULONG PropertyId,
+	ULONG ulSize,
+	BYTE pMin[],
+	BYTE pMax[],
+	BYTE pSteppingDelta[],
+	BYTE pDefault[])
+{
+	if (m_pKsControl == NULL) return E_POINTER;
+
+	// IHV may add code here, current stub just returns S_OK
+	HRESULT hr = S_OK;
+	return hr;
+}
+
+/************************************************************End of UVCXU class******************************************************************/
+
+/********************************************************Implementation of UVCCameraLibrary class************************************************/
+
 /*
 * Constructor of the class
 */
@@ -637,3 +939,74 @@ HRESULT UVCCameraLibrary::stopControling(KSPROPERTY_VIDCAP_CAMERACONTROL prop)
 		pCameraControl->Release();
 	return hr;
 }
+
+
+/*OSD menu tool*/
+HRESULT UVCCameraLibrary::osdMenuOpenClose()
+{
+	int nPos = 0;
+	return uvcxu.put_Property(UVC_1702C_XU_PLUG_CTRL_OPEN_CLOSE, ulUvcRedSize, (BYTE*)&nPos);
+}
+HRESULT UVCCameraLibrary::osdMenuEnter()
+{
+	int nPos = 0;
+	return uvcxu.put_Property(UVC_1702C_XU_PLUG_CTRL_OK, ulUvcRedSize, (BYTE*)&nPos);
+}
+HRESULT UVCCameraLibrary::osdMenuBack()
+{
+	int nPos = 0;
+	return uvcxu.put_Property(UVC_1702C_XU_PLUG_CTRL_BACK, ulUvcRedSize, (BYTE*)&nPos);
+}
+HRESULT UVCCameraLibrary::osdMenuUp()
+{
+	int nPos = 0;
+	return uvcxu.put_Property(UVC_1702C_XU_PLUG_CTRL_UP, ulUvcRedSize, (BYTE*)&nPos);
+}
+HRESULT UVCCameraLibrary::osdMenuDown()
+{
+	int nPos = 0;
+	return uvcxu.put_Property(UVC_1702C_XU_PLUG_CTRL_DOWN, ulUvcRedSize, (BYTE*)&nPos);
+}
+HRESULT UVCCameraLibrary::osdMenuLeft()
+{
+	int nPos = 0;
+	return uvcxu.put_Property(UVC_1702C_XU_PLUG_CTRL_LEFT, ulUvcRedSize, (BYTE*)&nPos);
+}
+HRESULT UVCCameraLibrary::osdMenuRight()
+{
+	int nPos = 0;
+	return uvcxu.put_Property(UVC_1702C_XU_PLUG_CTRL_RIGHT, ulUvcRedSize, (BYTE*)&nPos);
+}
+
+HRESULT UVCCameraLibrary::checkOSDMenu()
+{
+	HRESULT hr = E_FAIL;
+
+	if (pDeviceFilter == NULL)
+		return E_FAIL;
+
+	hr = uvcxu.QueryUvcXuInterface(pDeviceFilter, PROPSETID_XU_PLUG_IN_1700U, 0);
+	if (FAILED(hr))
+	{
+		return E_FAIL;
+	}
+
+	hr = E_FAIL;
+
+	if (uvcxu.get_PropertySize(UVC_1702C_XU_PLUG_CTRL_OPEN_CLOSE, &ulUvcRedSize) == S_OK)
+	{
+		BYTE* pbPropertyValue;
+		pbPropertyValue = new BYTE[ulUvcRedSize];
+		if (pbPropertyValue)
+		{
+			if (uvcxu.get_Property(UVC_1702C_XU_PLUG_CTRL_OPEN_CLOSE, ulUvcRedSize, (BYTE*)pbPropertyValue) == S_OK)
+			{
+				hr = S_OK;
+			}
+		}
+		delete pbPropertyValue;
+	}
+
+	return hr;
+}
+/********************************************************End of UVCCameraLibrary class************************************************************/
--- LibUVCCam/UVCCameraLibrary.h
+++ LibUVCCam/UVCCameraLibrary.h
@@ -3,11 +3,16 @@
 
 #include <windows.h>
 #include <iostream>
-
-#include <stdio.h>
+#include <strmif.h>
+#include <vidcap.h>
 #include <dshow.h>
 #include <ks.h>
-#include <Ksmedia.h>
+#include <ksproxy.h>
+#include <ksmedia.h>
+#include <uuids.h>
+#include <stdio.h>
+
+
 
 
 #ifdef UVCCAMERALIBRARY_EXPORTS
@@ -16,6 +21,112 @@
 #define UVCCAMERAAPI __declspec(dllimport)
 #endif
 #pragma once
+
+
+DEFINE_GUIDSTRUCT("A29E7641-DE04-47E3-8B2B-F4341AFF003B", PROPSETID_XU_H264);
+#define PROPSETID_XU_H264 DEFINE_GUIDNAMED(PROPSETID_XU_H264)
+
+DEFINE_GUIDSTRUCT("3C16A808-701C-49C7-AF98-2EC6C125B963", PROPSETID_XU_PLUG_IN);
+#define PROPSETID_XU_PLUG_IN DEFINE_GUIDNAMED(PROPSETID_XU_PLUG_IN)
+
+//DEFINE_GUIDSTRUCT("A7A6A934-3A4A-4351-85F4-042EF2CDCAAB", PROPSETID_XU_PLUG_IN_1700U);
+//#define PROPSETID_XU_PLUG_IN_1700U DEFINE_GUIDNAMED(PROPSETID_XU_PLUG_IN_1700U)
+DEFINE_GUIDSTRUCT("661DCD8C-ADB0-B344-8CB4-D335115FC18C", PROPSETID_XU_PLUG_IN_1700U);
+#define PROPSETID_XU_PLUG_IN_1700U DEFINE_GUIDNAMED(PROPSETID_XU_PLUG_IN_1700U)
+
+
+#ifndef ASSERT
+#define ASSERT(_x_) ((void)0)
+#endif
+
+#ifndef SAFE_RELEASE
+#define SAFE_RELEASE(x) if (x) { x->Release(); x = NULL; }
+#endif
+
+typedef enum {
+	UVC_XU_PLUG_IN_FLIP_CTRL = 1,
+	UVC_XU_PLUG_IN_OSD_RESTTING_CTRL,
+	UVC_XU_PLUG_IN_SAVE_STTING_CTRL,
+	UVC_XU_PLUG_IN_PRESET_CTRL,
+	UVC_XU_PLUG_IN_SLEEP_CTRL,
+	UVC_XU_PLUG_IN_RESET_PAN_TILT,
+	UVC_XU_PLUG_IN_RED,
+	UVC_XU_PLUG_IN_GREEN,
+	UVC_XU_PLUG_IN_BLUE,
+	UVC_XU_PLUG_IN_2D,
+}uvc_xu_plug_in_id_t;
+
+
+typedef enum {
+	UVC_1700U_XU_PLUG_IN_LDC = 3,
+	UVC_1700U_XU_PLUG_IN_FOCUSA,
+	UVC_1700U_XU_PLUG_IN_ZOOMLIMT,
+	UVC_1700U_XU_PLUG_STATUS = 8,
+	UVC_1700U_XU_PLUG_INFO,
+
+}uvc_1700u_xu_plug_in_id_t;
+
+
+typedef enum {
+	UVC_1702C_XU_PLUG_CTRL_UP = 1,
+	UVC_1702C_XU_PLUG_CTRL_DOWN,
+	UVC_1702C_XU_PLUG_CTRL_LEFT,
+	UVC_1702C_XU_PLUG_CTRL_RIGHT,
+	UVC_1702C_XU_PLUG_CTRL_OK,
+	UVC_1702C_XU_PLUG_CTRL_BACK,
+	UVC_1702C_XU_PLUG_CTRL_OPEN_CLOSE,
+
+}uvc_17002c_xu_plug_in_id_t;
+
+
+class UVCXU// : public IKsNodeControl
+{
+protected:
+	GUID    UvcXuGudi;
+	ULONG m_dwNodeId;
+	IKsControl* m_pKsControl = NULL;
+
+	//IKsPropertySet *m_pIKsPropertySet = NULL;
+
+public:
+
+	UVCXU();
+	virtual ~UVCXU();
+
+	HRESULT
+		QueryUvcXuInterface(
+			IBaseFilter* pDeviceFilter,
+			GUID extensionGuid,
+			DWORD FirstNodeId);
+
+	HRESULT FinalConstruct();
+
+	HRESULT get_Info(
+		ULONG ulSize,
+		BYTE pInfo[]);
+	HRESULT get_InfoSize(
+		ULONG* pulSize);
+	HRESULT get_PropertySize(
+		ULONG PropertyId,
+		ULONG* pulSize);
+	HRESULT get_Property(
+		ULONG PropertyId,
+		ULONG ulSize,
+		BYTE pValue[]);
+	HRESULT put_Property(
+		ULONG PropertyId,
+		ULONG ulSize,
+		BYTE pValue[]);
+	HRESULT get_PropertyRange(
+		ULONG PropertyId,
+		ULONG ulSize,
+		BYTE pMin[],
+		BYTE pMax[],
+		BYTE pSteppingDelta[],
+		BYTE pDefault[]);
+};
+
+
 
 class UVCCAMERAAPI UVCCameraLibrary
 {
@@ -67,6 +178,18 @@ public:
 	long getTilt();
 	long getZoom();
 	long getFocus();
+
+	/*OSD menu tool*/
+	HRESULT osdMenuOpenClose();
+	HRESULT osdMenuEnter();
+	HRESULT osdMenuBack();
+	HRESULT osdMenuUp();
+	HRESULT osdMenuDown();
+	HRESULT osdMenuLeft();
+	HRESULT osdMenuRight();
+
+	//This function must be called after connection to use other functions for osd menu
+	HRESULT checkOSDMenu();//check if the uvc camera supports osd menu
 private:
 
 	//base directshow filter
@@ -89,6 +212,15 @@ private:
 	//get value of the property
 	long getVal(CameraControlProperty prop);
 
+	/*OSD Menu*/
+	UVCXU uvcxu;
+	ULONG ulUvcRedSize;
+	ULONG ulUvcGreenSize;
+	ULONG ulUvcBlueSize;
+	ULONG ulUvc2dSize;
+
 };
+
+
 
 #endif
